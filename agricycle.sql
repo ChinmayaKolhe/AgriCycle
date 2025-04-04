2631f21ab57c00c33ddf24b35b6b97374dde8710
Chinmaya Kolhe <chinmayakolhe2005@gmail.com>
2025-04-04 19:13:39 +0530

    Added marketplace and notification in farmer dashboard

--- agricycle.sql
+++ agricycle.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: Apr 04, 2025 at 03:01 PM
+-- Generation Time: Apr 04, 2025 at 03:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
